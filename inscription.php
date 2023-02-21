ab08210e233f78b903bf96a7ea33ed7017a3831f
AndyDev77 <andyvespuce77550@gmail.com>
2023-02-21 16:00:08 +0100

    Page pour s'inscrire au sein de l'Usine-Ecole 4.0

---
+++ inscription.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+  <title>Inscription</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="./images/favicon.ico" type="image/svg+xml">
+  <!-- owl carousel css -->
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <!-- font awesome icons -->
+  <link rel="stylesheet" href="css/font-awesome.css">
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- main css -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <!--recapcha-->
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg fixed-top">
+    <!-- Brand -->
+    <a class="navbar-brand" href="index.php"><img src="./images/UE4.0.png" alt="Logo du Training Center 4.0" width="150">
+      <img src="./images/HASSTEC.PNG" alt="" width="80" style="margin-top: 78px;"></a>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" style="margin: 0 70px;">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Nous Connaitre
+          </a>
+
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="partenaires.php">Partenaires</a>
+            <a class="dropdown-item" href="inscription.php">Inscriptions</a>
+            <a class="dropdown-item" href="galerie.php">Galerie</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="apprenti.php">Accompagnement</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="actu.php">Actualités</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="learning.php">E-learning</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.paris-villaroche-training-center.parcours.pro/"
+            target="_blank">Plateforme</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link" href="soft-login.php">Soft-skills</a>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link" href="index.php#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+
+  </nav>
+
+  <!--ImageBackground-->
+  <section class="img-background"
+    style="background-image: url('./images/inscriptions/_1010463.jpg');  background-size: cover; height: 80vh; min-height: 300px; position: relative; color: var(--white); text-shadow: var(--shadow-black-100);">
+    <div class="color-overlay d-flex justify-content-center align-items-center" style="position: absolute; 
+      width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.2); ">
+      <h1 style="font-size: 4rem;">Inscriptions</h1>
+    </div>
+  </section>
+
+  <section class="faq section-padding" id="faq">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <div class="section-title">
+            <h2>S'inscrire au Training Center 4.0</h2>
+          </div>
+        </div>
+      </div>
+      <br>
+      <br>
+      <div class="row justify-content-center">
+        <div class="col-lg-10">
+          <div id="accordion">
+            <div class="accordion-item">
+              <div class="accordion-header collapsed" data-toggle="collapse" data-target="#collapse-01">
+                <h3>Quels sont les conditions d'inscriptions au Training Center 4.0 ?</h3>
+              </div>
+              <div class="collapse" id="collapse-01" data-parent="#accordion">
+                <div class="accordion-body">
+                  <p><i class="fas fa-check"></i>&nbsp;&nbsp;À partir de 18 ans et sans limite d'âge.</p>
+                  <p><i class="fas fa-check"></i>&nbsp;&nbsp;Gratuit pour les demandeurs d'emploi et les jeunes de moins de 26 ans.</p>
+                  <p><i class="fas fa-check"></i>&nbsp;&nbsp;Maintient de l'inscription au Training Center 4.0 en cas de retour à l'emploi.</p>
+                  <p><i class="fas fa-check"></i>&nbsp;&nbsp;Inscription tout au long de l'année pour 12 mois de date à date.</p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <div class="accordion-header collapsed" data-toggle="collapse" data-target="#collapse-02">
+                <h3>Quel niveau d'étude pour s'inscrire au Training Center 4.0 ?</h3>
+              </div>
+              <div class="collapse" id="collapse-02" data-parent="#accordion">
+                <div class="accordion-body">
+                  <p><i class="fas fa-plane-departure"></i>&nbsp;&nbsp; Ouverture d'esprit </p>
+                  <p><i class="fas fa-plane-departure"></i>&nbsp;&nbsp; Capacité à travailler  </p>
+                  <p><i class="fas fa-plane-departure"></i>&nbsp;&nbsp; Capacité à échanger  </p>
+                  <p><i class="fas fa-plane-departure"></i>&nbsp;&nbsp; Persévérance </p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <div class="accordion-header collapsed" data-toggle="collapse" data-target="#collapse-03">
+                <h3>Comment être pour s'inscrire au Training Center 4.0 ?</h3>
+              </div>
+              <div class="collapse" id="collapse-03" data-parent="#accordion">
+                <div class="accordion-body">
+                  <p><i class="fas fa-plane-arrival"></i>&nbsp;&nbsp; Être impliqué</p>
+                  <p><i class="fas fa-plane-arrival"></i>&nbsp;&nbsp;  Être motivé</p>
+                  <p><i class="fas fa-plane-arrival"></i>&nbsp;&nbsp; Être à l'écoute</p>
+                  <p><i class="fas fa-plane-arrival"></i>&nbsp;&nbsp; Être disponible</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="chevron-btn">
+        <a href="#inscription"><i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+      </div>
+    </div>
+  </section>
+
+  <section class="inscription section-padding" id="inscription">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-5 d-flex align-items-center justify-content-center">
+          <div class="inscription-img">
+            <img src="./images/inscriptions/TC2.jpg" alt="">
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-7">
+          <div class="section-title">
+            <h3>Il vous suffit d'être passionné et autonome.</h3>
+          </div>
+          <br>
+          <div class="inscription-text">
+            <p>Votre parcours est strictement individuel et personnel. Un bilan de compétences des modules validés vous sera communiqué à tout moment de votre parcours.</p>
+            <br>
+            <div class="inscription-btn">
+              <a href="index.php#contact" class="btn btn-2">Inscrivez-Vous</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- BAND -->
+
+  <section class="band ">
+    <div class="slider">
+      <div class="slide-track">
+        <div class="slide">
+          <img src="./images/img-partenaires/logo-ue.jpg" height="100" width="150" alt="">
+        </div>
+        <div class="slide">
+          <img src="./images/img-partenaires/REGION_IDF.jpg" height="100" width="200" alt="">
+        </div>
+
+        <div class="slide">
+          <img src="./images/img-partenaires/pole-emploi-2.jpg" height="100" width="130" alt="">
+        </div>
+        <div class="slide">
+          <img src="./images/img-partenaires/melun.jpg" height="100" width="110" alt="">
+        </div>
+        <div class="slide">
+          <img src="./images/img-partenaires/PREF77.jpg" height="100" width="100" alt="">
+        </div>
+        <div class="slide">
+          <img src="./images/img-partenaires/AIF.jpg" height="100" width="120" alt="">
+        </div>
+
+        <div class="slide">
+          <img src="./images/img-partenaires/FF.png" height="100" width="190" alt="">
+        </div>
+        <div class="slide">
+          <img src="./images/img-partenaires/EOZ.jpg" height="100" width="190" alt="">
+        </div>
+        <div class="slide">
+          <img src="./images/img-partenaires/LdV.png" height="100" width="200" alt="">
+        </div>
+        <div class="slide">
+          <img src="./images/img-partenaires/htec.jpg" height="100" width="200" alt="">
+        </div>
+        <div class="slide">
+          <img src="./images/img-partenaires/cfa.png" height="100" width="200" alt="">
+        </div>
+        <div class="slide">
+          <img src="./images/img-partenaires/cam-system.png" height="100" width="200" alt="">
+        </div>
+        <div class="slide">
+          <img src="./images/img-partenaires/jbplogo.jpg" height="100" width="200" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+          <div class="footer-col">
+            <h3>Training Center 4.0</h3>
+            <p class="text-footer">Pôle de ressources et d'accompagnement vers les métiers mécaniques
+              aéronautiques et industriels soutenu par l'Europe.</p>
+            <div class="footer-links">
+              <a href="https://m.facebook.com/trainingcentersympav/" target="_blank" class="footer__social">
+                <i class="fa fa-facebook-official"></i>
+              </a>
+              <a href="https://twitter.com/TCenter4_0/" target="_blank" class="footer__social">
+                <i class="fa fa-twitter"></i>
+              </a>
+              <a href="https://www.instagram.com/trainingcenter4.0/" target="_blank" class="footer__social">
+                <i class="fa fa-instagram"></i>
+              </a>
+              <a href="https://www.youtube.com/channel/UCqan073e4fkLA_HtG_uWk8Q" target="_blank" class="footer__social">
+                <i class="fa fa-youtube-play"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/tcenter-sympav-28bb571a3/" target="_blank" class="footer__social">
+                <i class="fa fa-linkedin"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="footer-col props">
+            <h3>À propos</h3>
+            <ul>
+              <li><a href="apprenti.php">Accompagnement</a></li>
+              <li><a href="learning.php">E-Learning</a></li>
+              <li><a href="actu.php">Actualités</a></li>
+              <li><a href="https://www.paris-villaroche-training-center.parcours.pro/">Plateforme</a></li>
+              <!-- <li><a href="#">Soft-Skills</a></li> -->
+              <li><a href="index.php#contact">Contact</a></li>
+
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="footer-col">
+            <h3>Informations</h3>
+            <p>Aérodrome de Melun Villaroche 77950 Montereau-sur-le-Jard</p>
+            <br>
+            <a class="mail" href="mailto:trainingcenter@parisvillaroche.com" class="footer__link">trainingcenter@ <br>
+              parisvillaroche.com
+            </a>
+            <br>
+            <a class="mail" href="tel:+33757079455" class="footer__link">01 60 68 11 44</a>
+          </div>
+        </div>
+
+         <!-- <div class="col-lg-4 col-md-6">
+          <div class="footer-col">
+            <h3>Nos Politiques</h3>
+            <ul>
+              <li><a href="#">Politique d’utilisation des cookies</a></li>
+              <li><a href="#">Mentions légales</a></li>
+              <li><a href="#">Politique de confidentialité</a></li>
+            </ul>
+          </div>
+        </div> -->
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <p class="copyright-text">&copy;2023 Training Center 4.0. Tous droits réservés.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--========== SCROLL UP ==========-->
+  <a href="#" class="scrollup scroll-up">
+    <i class="fa fa-arrow-up scrollup__icon" aria-hidden="true"></i>
+  </a>
+
+  <!-- jquery js -->
+  <script src="js/jquery.min.js"></script>
+  <!-- popper js -->
+  <script src="js/popper.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- owl carousel js -->
+  <script src="js/owl.carousel.min.js"></script>
+  <!-- ScrollIt js -->
+  <script src="js/scrollIt.min.js"></script>
+  <!-- main js -->
+  <script src="js/main.js"></script>
+</body>
+
+</html>
